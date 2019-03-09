18cf15a24fb7580966ccd8885908444125dd4f63
jsrn <james.srn@gmail.com>
2019-03-09 11:14:47 +0000

    Finished "It Doesn't Have To Be Crazy At Work"

--- generate.rb
+++ generate.rb
@@ -4,7 +4,7 @@ cover_list_html = Dir["covers/*"].map do |book|
   %(<div><img src="#{book}" alt=""></div>)
 end
 
-generated_html = template.sub!("BOOKSGOHERE", cover_list_html.join(""))
+generated_html = template.sub!("BOOKSGOHERE", cover_list_html.join("\n"))
 
 File.write("index.html", generated_html)
 
