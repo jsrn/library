7f0245bd185b9f096e7bef3bb986140adec31944
jsrn <james.srn@gmail.com>
2019-03-09 12:52:30 +0000

    Let's do it with a grid

--- generate.rb
+++ generate.rb
@@ -2,10 +2,12 @@ require "fastimage"
 
 template = File.read("index.html.template")
 
-cover_list_html = Dir["covers/*"].map do |book|
-  _, height = FastImage.size(book)
+COLOURS = %w(red green blue magenta)
 
-  %(<div><img class="lazy" style="min-width: 300px; min-height: #{height}px;" data-src="#{book}" alt=""></div>)
+cover_list_html = Dir["covers/*"].map do |book|
+  width, height = FastImage.size(book)
+
+  %(<img class="lazy" style="width: #{width}px; height: #{height}px;" data-src="#{book}" alt="">)
 end
 
 generated_html = template.sub!("BOOKSGOHERE", cover_list_html.join("\n"))
