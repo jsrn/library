9b262c436d8dc851bdfb19b2b6d001c1fb25403c
jsrn <james.srn@gmail.com>
2019-03-09 12:30:06 +0000

    Some technical notes and styling

--- generate.rb
+++ generate.rb
@@ -2,12 +2,10 @@ require "fastimage"
 
 template = File.read("index.html.template")
 
-COLOURS = %w(#ace8dc #b5eecd #b7dbf3 #ddc5e7 #faeba6 #f6d2ae #f8c1ba)
-
 cover_list_html = Dir["covers/*"].map do |book|
   _, height = FastImage.size(book)
 
-  %(<div><img class="lazy" style="width: 300px; height: #{height}px; background: #{COLOURS.sample}" data-src="#{book}" alt=""></div>)
+  %(<div><img class="lazy" style="min-width: 300px; min-height: #{height}px;" data-src="#{book}" alt=""></div>)
 end
 
 generated_html = template.sub!("BOOKSGOHERE", cover_list_html.join("\n"))
