4b60a960b752d5cc944da3e9819ebf8eb71d0695
jsrn <james.srn@gmail.com>
2019-03-09 12:02:47 +0000

    Hugely reduced file size and added lazy loading

--- generate.rb
+++ generate.rb
@@ -1,11 +1,14 @@
+require "fastimage"
+
 template = File.read("index.html.template")
 
 cover_list_html = Dir["covers/*"].map do |book|
-  %(<div><img src="#{book}" alt=""></div>)
+  _, height = FastImage.size(book)
+
+
+  %(<div><img class="lazy" style="width: 300px; height: #{height}px;" data-src="#{book}" alt=""></div>)
 end
 
 generated_html = template.sub!("BOOKSGOHERE", cover_list_html.join("\n"))
 
 File.write("index.html", generated_html)
-
-puts template
