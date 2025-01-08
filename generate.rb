56f446dfea6bee7a384a1a2af236b53837827dd6
jsrn <james@jsrn.net>
2025-01-08 09:12:02 +0000

    Some style tweaks

--- generate.rb
+++ generate.rb
@@ -9,13 +9,10 @@ total_size = 0
 
 cover_list_html = Dir["covers/*"].sort.map do |book|
   width, height = FastImage.size(book)
-  total_size += File.size(book)
 
   %(<img loading="lazy" style="width: #{width}px; height: #{height}px;" src="#{book}" alt="">)
 end
 
-formatted_size = ((total_size.to_f / 1024) / 1024).round(2)
-
-generated_html = template.sub("BOOKSGOHERE", cover_list_html.join("\n")).sub("IMAGE_SIZE", formatted_size.to_s)
+generated_html = template.sub("BOOKSGOHERE", cover_list_html.join("\n"))
 
 File.write("index.html", generated_html)
